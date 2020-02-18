6dd16f95c986cb58532c61da8a3e16e92f294942
Nikhil Kulkarni <47928380+nxk180002@users.noreply.github.com>
2020-02-17 18:33:55 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var imgs = document.querySelectorAll('.hb');
+
+for (var i = 0; i < imgs.length; i++) {
+  imgs[i].onclick = toggleAnimation;
+  imgs[i].style.webkitAnimationPlayState = 'running';
+}
+
+function toggleAnimation() {
+  var style;
+  for (var i = 0; i < imgs.length; i++) {
+    style = imgs[i].style;
+    if (style.webkitAnimationPlayState === 'running') {
+      style.webkitAnimationPlayState = 'paused';
+      document.body.className = 'paused';
+    } else {
+      style.webkitAnimationPlayState = 'running';
+      document.body.className = '';
+    }
+  }
+}
